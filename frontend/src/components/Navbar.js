92662daca6dde0ec5656c20f0f96e25045ca36db
shonesjn <shonesajanstjohns@gmail.com>
2026-03-20 22:04:13 +0530

    Update navbar with SAINTGITS theme

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import saintgitsLogo from '../assets/images.png'; // adjust path as needed
 
 const Navbar = ({ onToggleSidebar }) => {
   return (
@@ -11,34 +12,17 @@ const Navbar = ({ onToggleSidebar }) => {
         </button>
         
         <div className="logo">
-          <img src="https://tse4.mm.bing.net/th/id/OIP.OQ6ucQ9Mfhg3t1WngxT2UgHaEM?pid=Api&P=0&h=180" alt="Logo" className="logo-img" />
+          <img src={saintgitsLogo} alt="Saintgits Logo" className="logo-img" />
         </div>
       </div>
       
       <div className="navbar-center">
         <h1 className="navbar-title">IOT Dashboard</h1>
       </div>
-      
-      {/* <div className="navbar-right">
-        <div className="notification-icon">
-          <svg 
-            width="24" 
-            height="24" 
-            viewBox="0 0 24 24" 
-            fill="none" 
-            stroke="currentColor" 
-            strokeWidth="2"
-            strokeLinecap="round" 
-            strokeLinejoin="round"
-          >
-            <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-            <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-          </svg>
-          <span className="notification-badge">3</span>
-        </div>
-      </div> */}
+
     </nav>
   );
 };
 
 export default Navbar;
+
